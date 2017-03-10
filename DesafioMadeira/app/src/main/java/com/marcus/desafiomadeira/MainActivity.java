eb92d8ab44ff6d00778a7e46a1febf8b4f0506d7
Marcus <marcus_amaral@msn.com>
2017-03-09 23:45:43 -0300

    Busca do mock e conversão do resultado para JSONObject

--- DesafioMadeira/app/src/main/java/com/marcus/desafiomadeira/MainActivity.java
+++ DesafioMadeira/app/src/main/java/com/marcus/desafiomadeira/MainActivity.java
@@ -1,24 +1,28 @@
 package com.marcus.desafiomadeira;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 
-import com.marcus.desafiomadeira.controller.RestController;
+import com.marcus.desafiomadeira.controller.JsonFromURL;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_main);
 
-        RestController restController = new RestController();
+        new JsonFromURL(new JsonFromURL.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+                System.out.println(output);
+            }
+        }).execute("http://private-2b254a-desafiomadeira.apiary-mock.com/questions");
 
-
-
-        TextView texto = (TextView) findViewById(R.id.texto);
-
-        texto.setText(restController.buscaItinerario());
     }
 }
--- DesafioMadeira/app/src/main/java/com/marcus/desafiomadeira/controller/IRestController.java
+++
@@ -1,9 +0,0 @@
-package com.marcus.desafiomadeira.controller;
-
-/**
- * Created by Marcus on 08/03/2017.
- */
-
-public interface IRestController {
-    String buscaItinerario ();
-}
