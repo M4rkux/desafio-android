f8a1028a2791dacbd4b0d2993cf25cfd897dc46d
Marcus <marcus_amaral@msn.com>
2017-03-17 02:11:57 -0300

    Capturando latitude e longitude com o gps, adicionando foto, alterando layout do desenho (quebrando a funcionalidade do desenho).

--- DesafioMadeira/app/src/main/java/com/marcus/desafiomadeira/view/DetalhesActivity.java
+++ DesafioMadeira/app/src/main/java/com/marcus/desafiomadeira/view/DetalhesActivity.java
@@ -1,15 +1,27 @@
 package com.marcus.desafiomadeira.view;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +30,17 @@ import com.marcus.desafiomadeira.model.Item;
 import com.marcus.desafiomadeira.model.Itinerario;
 
 import java.io.Serializable;
+import java.security.Provider;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DetalhesActivity extends AppCompatActivity implements Serializable {
+
+    public static final int OPCAO_ASSINATURA = 0;
+    private static final int OPCAO_FOTO = 1;
+    private static final int OPCAO_QR_CODE = 2;
+    private static final int OPCAO_IDA = 3;
+    private static final int OPCAO_CHEGADA = 4;
 
     private Intent it;
     Itinerario itinerarioItem;
@@ -81,8 +100,8 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
             @Override
             public void onClick(View v) {
                 Intent intentMaps = new Intent(android.content.Intent.ACTION_VIEW,
-                    Uri.parse("geo:0,0?q=" + itinerarioItem.getEndereco().getLatitude() + "," +
-                            itinerarioItem.getEndereco().getLongitude() + "( " + itinerarioItem.getTipo() + ": " + itinerarioItem.getEndereco().getBairro() +")"));
+                        Uri.parse("geo:0,0?q=" + itinerarioItem.getEndereco().getLatitude() + "," +
+                                itinerarioItem.getEndereco().getLongitude() + "( " + itinerarioItem.getTipo() + ": " + itinerarioItem.getEndereco().getBairro() + ")"));
                 startActivity(intentMaps);
             }
         });
@@ -94,9 +113,9 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
                 alertDialog.setTitle("Itens");
                 final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(DetalhesActivity.this, android.R.layout.select_dialog_singlechoice);
 
-                final List<Item> itens =itinerarioItem.getItens();
+                final List<Item> itens = itinerarioItem.getItens();
 
-                for (Item item: itens) {
+                for (Item item : itens) {
                     arrayAdapter.add(item.getTitulo());
                 }
                 alertDialog.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
@@ -112,7 +131,7 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
                         builderInner.setMessage(message);
                         builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialog,int which) {
+                            public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
                             }
                         });
@@ -155,7 +174,7 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
             public void onClick(View v) {
                 AlertDialog alertDialog = new AlertDialog.Builder(DetalhesActivity.this).create();
                 alertDialog.setTitle("Deseja informar que foi entregue?");
-                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Sim",  new DialogInterface.OnClickListener() {
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Sim", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
@@ -195,7 +214,7 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
                         builderInner.setMessage("Ocorrência registrada");
                         builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialog,int which) {
+                            public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
                             }
                         });
@@ -227,16 +246,39 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
                 alertDialog.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        AlertDialog.Builder builderInner = new AlertDialog.Builder(DetalhesActivity.this);
-                        builderInner.setTitle(arrayAdapter.getItem(which));
-                        builderInner.setMessage("Prova inserida");
-                        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog,int which) {
-                                dialog.dismiss();
-                            }
-                        });
-                        builderInner.show();
+                        String gpsInfo;
+                        String message;
+                        String[] info;
+                        Intent it;
+
+                        switch (which) {
+                            case OPCAO_ASSINATURA:
+                                it = new Intent(getApplicationContext(), AssinaturaActivity.class);
+                                startActivityForResult(it, OPCAO_ASSINATURA);
+                                break;
+                            case OPCAO_FOTO:
+                                it = new Intent("android.media.action.IMAGE_CAPTURE");
+                                startActivityForResult(it, OPCAO_FOTO);
+                                break;
+                            case OPCAO_QR_CODE:
+                                break;
+                            case OPCAO_IDA:
+                                message = "Registrando ida ao local, a partir do ponto: \n\n";
+                                gpsInfo = getGpsInfo();
+                                info = gpsInfo.split(" ");
+                                message += "Latitude: "+info[0]+"\n";
+                                message += "Longitude: "+info[1];
+                                showDialog(message, "Ida ao local");
+                                break;
+                            case OPCAO_CHEGADA:
+                                message = "Sua posição atual é: \n\n";
+                                gpsInfo = getGpsInfo();
+                                info = gpsInfo.split(" ");
+                                message += "Latitude: "+info[0]+"\n";
+                                message += "Longitude: "+info[1];
+                                showDialog(message, "Chegada ao local");
+                                break;
+                        }
                     }
                 });
                 alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -248,5 +290,86 @@ public class DetalhesActivity extends AppCompatActivity implements Serializable 
                 alertDialog.show();
             }
         });
+    }
+
+    protected String getGpsInfo() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "";
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        return Double.toString(longitude) + " " + Double.toString(latitude);
+    }
+
+    protected void showDialog(String message) {
+        AlertDialog.Builder builderInner = new AlertDialog.Builder(DetalhesActivity.this);
+        builderInner.setMessage(message);
+        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog,int which) {
+                dialog.dismiss();
+            }
+        });
+        builderInner.show();
+    }
+
+    protected void showDialog(String message, String title) {
+        AlertDialog.Builder builderInner = new AlertDialog.Builder(DetalhesActivity.this);
+        builderInner.setTitle(title);
+        builderInner.setMessage(message);
+        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog,int which) {
+                dialog.dismiss();
+            }
+        });
+        builderInner.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case OPCAO_ASSINATURA:
+                if (resultCode == RESULT_OK) {
+                    showDialog("Assinatura capturada");
+                }
+                break;
+            case OPCAO_FOTO:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap image = (Bitmap) extras.getParcelable("data");
+
+                    final Dialog settingsDialog = new Dialog(this);
+                    settingsDialog.setTitle("Foto Salva");
+                    settingsDialog.setContentView(getLayoutInflater().inflate(R.layout.image_layout
+                            , null));
+                    settingsDialog.findViewById(R.id.bt_ok).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            settingsDialog.dismiss();
+                        }
+                    });
+
+                    DisplayMetrics metrics = getResources().getDisplayMetrics();
+                    int width = metrics.widthPixels;
+                    int height = metrics.heightPixels;
+                    settingsDialog.getWindow().setLayout((6 * width) / 7, (4 * height) / 5);
+                    settingsDialog.show();
+
+                    ImageView imageview = (ImageView) settingsDialog.findViewById(R.id.ImageView01);
+                    imageview.setImageBitmap(image);
+                }
+                break;
+        }
     }
 }
